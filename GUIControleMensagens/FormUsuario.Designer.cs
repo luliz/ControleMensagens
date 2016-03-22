d86b5a392b5f96832409fce30f3ef1d2135b61f8
Luccas Mateus <Luccasmmg@gmail.com>
2016-03-22 10:07:08 -0300

    Yo

---
+++ GUIControleMensagens/FormUsuario.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GUIControleMensagens
+{
+    partial class FormUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Gravar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Excluir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(296, 123);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(177, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(177, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Novo";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // FormUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 230);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FormUsuario";
+            this.Text = "FormUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ GUIControleMensagens/FormUsuario.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+using Controller;
+namespace GUIControleMensagens 
+
+
+{
+    public partial class FormUsuario : Form
+    {
+        private ControllerUsuario controle;
+
+        public FormUsuario()
+        {
+            InitializeComponent();
+            controle = new ControllerUsuario();
+        }
+
+        private void novo()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "0";
+        }
+
+        private void atualizaGrid()
+        {
+            dataGridView1.DataSource = controle.ListarUsuarios();
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void FormCliente_Load(object sender, EventArgs e)
+        {
+            atualizaGrid();
+            novo();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                List<Usuario> lista = (List<Usuario>)dataGridView1.DataSource;
+                Usuario cliente = lista[dataGridView1.CurrentRow.Index];
+                textBox1.Text = cliente.Nome;
+                textBox2.Text = cliente.Email;
+                textBox3.Text = cliente.IDUsuario.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            novo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = textBox1.Text;
+            usuario.Email = textBox2.Text;
+            usuario.IDUsuario = Convert.ToInt32(textBox3.Text);
+
+            if (controle.GravarUsuario(usuario))
+            {
+                MessageBox.Show("Usuario salvo com sucesso");
+                novo();
+                atualizaGrid();
+            }
+            else
+            {
+                MessageBox.Show("Erro ao salvar usuario");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int idUsuario = Convert.ToInt32(textBox3.Text);
+            if (idUsuario != 0)
+            {
+                if (controle.ExcluirUsuario(idUsuario))
+                {
+                    MessageBox.Show("Usuario excluido com sucesso");
+                    novo();
+                    atualizaGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Erro ao excluir Usuario");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione um Usuario para exclusão");
+            }
+        }
+    }
+}
